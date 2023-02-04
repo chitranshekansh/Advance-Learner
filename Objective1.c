21dfc74fff93984492bf82435b5831a80f2391fe
Ekansh Arun Srivastava <104249396+chitranshekansh@users.noreply.github.com>
2023-02-04 12:18:57 +0530

    Create Objective1.c

---
+++ Objective1.c
@@ -0,0 +1,14 @@
+//to find greatest in two
+#include <stdio.h>
+
+int main() {
+int a,b;
+printf("Enter Two Numbers:");
+scanf("%d%d",&a,&b);
+if(a>b)
+printf("%d is greater",a);
+else
+printf("%d is greater",b);
+
+return 0;
+}
